72fbee562921c20b576160733f3b5adb93be2568
nurhusein <nurabd@student.chalmers.se>
2019-12-10 15:26:29 +0100

    better view for the selet categori section

--- src/components/categoryList.jsx
+++ src/components/categoryList.jsx
@@ -3,19 +3,23 @@ import React, {Component} from 'react';
 class CategoryList extends Component {
     render() {
         return (
-            <ul className="list-group list-group-horizontal" Style="width:100%">
-                <li className="list-group-item">Bilar</li>
-                <li className="list-group-item">Jobb</li>
-                <li className="list-group-item">  För hemmet</li>
-                <li className="list-group-item">Bostad</li>
-                <li className="list-group-item">resor</li>
-                <li className="list-group-item">klöder</li>
-                <li className="list-group-item">fritid och hoppy</li>
-                <li className="list-group-item">Elektronik</li>
-                <li className="list-group-item">övrigt</li>
+            <div>
+                <div Style=" height:50px;"></div>
 
+                <div className="row">
+                    <div className="col-3 list-group-item">Bilar</div>
+                    <div className="col-3 list-group-item">Bilar</div>
+                    <div className="col-3 list-group-item">Bilar</div>
+                    <div className="col-3 list-group-item">Bilar</div>
+                </div>
+                <div className="row">
+                    <div className="col-3 list-group-item">Bilar</div>
+                    <div className="col-3 list-group-item">Bilar</div>
+                    <div className="col-3 list-group-item">Bilar</div>
+                    <div className="col-3 list-group-item">Bilar</div>
+                </div>
+            </div>
 
-            </ul>
         );
     }
 }
