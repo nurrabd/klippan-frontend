b890765287c66a904c770f9f14d59e348cff7ff8
nurhusein <nurabd@student.chalmers.se>
2019-12-10 16:20:30 +0100

    better view for the search  section

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -19,39 +19,48 @@ class Search extends Component {
             <div className="container ">
                 <div Style="height:20px"/>
                 <div className="row justify-content-center">
-                    <div className="col-md-6  ">
+                    <div className="col-md-10  ">
 
                         <Form className=" "  onSubmit={this.onSubmit}>
-
-                        <Form.Group>
-
-
-                            <Form.Control placeholder="municipality"
-                                          type="text"
-                                          className=" form-control-md"
-                                          Style="width:100%"
-                                          name="municipality"
-                                          value={this.state.municipality}
-                                          onChange={this.onChange}/>
+                        <div className="row">
+                            <div className="col-5"><Form.Group>
 
 
-                        </Form.Group>
+                                <Form.Control placeholder="municipality"
+                                              type="text"
+                                              className=" form-control-lg"
+                                              Style="width:100%"
+                                              name="municipality"
+                                              value={this.state.municipality}
+                                              onChange={this.onChange}/>
 
 
-                        <Form.Group className="  border">
-                            <Form.Control placeholder="category"
-                                          type="text"
-                                          className=" form-control-md "
-                                          name="category"
-                                          Style="width:100%"
-                                          value={this.state.category}
-                                          onChange={this.onChange}/>
+                            </Form.Group></div>
+                            <div className="col-5   ">
+                                <Form.Group className="   ">
+                                    <Form.Control placeholder="category"
+                                                  type="text"
+                                                  className=" form-control-lg "
+                                                  name="category"
+                                                  Style="width:100%; "
+                                                  value={this.state.category}
+                                                  onChange={this.onChange}/>
 
 
-                        </Form.Group>
-                            <Button className="login" type="submit" Style="width:100%">
-                                Search
-                            </Button>
+                                </Form.Group>
+                            </div>
+                            <div className="col-2">
+
+                                <Button className="login" type="submit" Style="width:70%; height:70%; margin-top:2px">
+                                    Search
+                                </Button>
+                            </div>
+                        </div>
+
+
+
+
+
 
                         </Form>
                     </div>
